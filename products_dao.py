9e88207a69324c4db19d50594eff94495fae7ecc
Nihal rahoof <nihalrahoof123@gmail.com>
2024-08-09 11:46:42 +0530

    Update products_dao.py

--- products_dao.py
+++ products_dao.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from sql_connection import get_sql_connection
-
+import os
 app = Flask(__name__)
 
 
@@ -83,4 +83,5 @@ def delete_product_route():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
