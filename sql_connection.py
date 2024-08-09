aec37c00a952e2aead6fb57333064f4cd7f0a066
Nihal rahoof <nihalrahoof123@gmail.com>
2024-08-09 11:03:19 +0530

    Update sql_connection.py

--- sql_connection.py
+++ sql_connection.py
@@ -1,15 +1,19 @@
-import datetime
+import os
 import mysql.connector
 
 __cnx = None
-
 
 def get_sql_connection():
     print("Opening mysql connection")
     global __cnx
 
     if __cnx is None:
-        __cnx = mysql.connector.connect(user='root', password='FathimaRahoof@123',
-                                        database='quotation_mgmt')
+        __cnx = mysql.connector.connect(
+            host=os.getenv('mysql.railway.internal'),
+            user=os.getenv('root'),
+            password=os.getenv('kuQGNpXVYhtiVnEFPSEnjXaLFDjouXLG'),
+            database=os.getenv('railway'),
+            port=os.getenv('DB_PORT', 3306)  # Default to 3306 if not set
+        )
 
     return __cnx
